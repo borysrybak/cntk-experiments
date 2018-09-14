4c9bca222074060462a4198a9ec01c6721206383
Borys Rybak <borys.rybak@gmail.com>
2018-09-14 14:00:58 +0200

    :recycle: Change name of variables for readability

--- 2. Concepts/input_output.py
+++ 2. Concepts/input_output.py
@@ -30,50 +30,50 @@ def main():
     Y = C.input_variable(output_node_dim, np.float32)
 
     print('\nCreating a 4-2-3 tanh-softmax neural network')
-    h = C.layers.Dense(hidden_node_dim, activation = C.tanh, name = 'hidLayer')(X)
-    o = C.layers.Dense(output_node_dim, activation = C.softmax, name = 'outLayer')(h)
-    nnet = o
+    hidden_layer = C.layers.Dense(hidden_node_dim, activation = C.tanh, name = 'hidLayer')(X)
+    output_layer = C.layers.Dense(output_node_dim, activation = C.softmax, name = 'outLayer')(hidden_layer)
+    neural_network = output_layer # or neural_network = C.ops.alias(output_layer) 
 
     print('\nSetting weights and bias values')
-    ih_wts = np.array([[0.01, 0.02],
+    input_to_hidden_weights = np.array([[0.01, 0.02],
                        [0.03, 0.04],
                        [0.05, 0.06],
                        [0.07, 0.08]], dtype = np.float32)
 
-    h_biases = np.array([0.09, 0.10])
+    hidden_biases = np.array([0.09, 0.10])
 
-    ho_wts = np.array([[0.11, 0.12, 0.13],
+    hidden_to_output_weights = np.array([[0.11, 0.12, 0.13],
                        [0.14, 0.15, 0.16]], dtype = np.float32)
 
-    o_biases = np.array([0.17, 0.18, 0.19], dtype = np.float32)
+    output_biases = np.array([0.17, 0.18, 0.19], dtype = np.float32)
 
-    h.hidLayer.W.value = ih_wts
-    h.hidLayer.b.value = h_biases
-    o.outLayer.W.value = ho_wts
-    o.outLayer.b.value = o_biases
+    hidden_layer.hidLayer.W.value = input_to_hidden_weights
+    hidden_layer.hidLayer.b.value = hidden_biases
+    output_layer.outLayer.W.value = hidden_to_output_weights
+    output_layer.outLayer.b.value = output_biases
 
     print('\nSet the input-hidden weights to: ')
-    print(h.hidLayer.W.value)
+    print(hidden_layer.hidLayer.W.value)
     print('Set the hidden node biases to: ')
-    print(h.hidLayer.b.value)
+    print(hidden_layer.hidLayer.b.value)
     print('Set the hidden-output weights to: ')
-    print(o.outLayer.W.value)
+    print(output_layer.outLayer.W.value)
     print('Set the output node biases to: ')
-    print(o.outLayer.b.value)
+    print(output_layer.outLayer.b.value)
 
     print('\nSetting input values to (1.0, 2.0, 3.0, 4.0)')
-    x_vals = np.array([1.0, 2.0, 3.0, 4.0], dtype = np.float32)
+    input_values = np.array([1.0, 2.0, 3.0, 4.0], dtype = np.float32)
 
     np.set_printoptions(formatter = {'float': '{: 0.4f}'.format})
     print('\nFeeding input values to hidden layer only ')
-    h_vals = h.eval({X: x_vals})
+    hidden_values = hidden_layer.eval({X: input_values})
     print('Hidden node values:')
-    print(h_vals)
+    print(hidden_values)
 
     print('\nFeeding input values to entire network ')
-    y_vals = nnet.eval({X: x_vals})
+    output_values = neural_network.eval({X: input_values})
     print('Output node values:')
-    print(y_vals)
+    print(output_values)
 
     ##########################
     print('\n ### End demo\n')
